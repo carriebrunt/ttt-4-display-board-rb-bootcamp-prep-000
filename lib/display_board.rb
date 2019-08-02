860c92eb9a4bd719cebbb99f4383ed8d97cb650a
Carrie Brunt <cdpb18@gmail.com>
2019-08-02 17:17:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts row = "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts separator = "-----------"
-  puts row
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts separator
-  puts row
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
